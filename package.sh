1f9dc78cc21241bd0e5f40af2cedc28bd6a82898
smackem <sm4ckem@outlook.com>
2020-03-27 11:58:50 +0100

    - added packaging script for jdk14/jpackage

---
+++ package.sh
@@ -0,0 +1,14 @@
+# package.sh builds a standalone jobotwar macosx or linux package
+# remember to use jdk 14
+
+JAR=jobotwar-gui-1.0-SNAPSHOT.jar
+VERSION='1.1'
+
+# set java release to 14
+sed -i "" "s/\<maven\.compiler\.release>13\</<maven.compiler.release>14</" pom.xml
+# rebuild jars
+mvn clean package
+# package jars in dmg
+jpackage -i jobotwar-gui/target/ -n jobotwar --main-class net.smackem.jobotwar.gui.JarMain --main-jar $JAR --java-options '--enable-preview' --app-version $VERSION
+# reset java release
+sed -i "" "s/\<maven\.compiler\.release>14\</<maven.compiler.release>13</" pom.xml 
