3890ec8aee338d49fc8664e0996739614d4e7a8d
smackem <sm4ckem@outlook.com>
2020-04-16 15:08:36 +0200

    - updated package.sh

--- package.sh
+++ package.sh
@@ -2,13 +2,17 @@
 # remember to use jdk 14
 
 JAR=jobotwar-gui-1.0-SNAPSHOT.jar
-VERSION='1.1'
+VERSION='1.2'
+
+# note:
+# macosx (.dmg) works fine
+# ubuntu (.deb) installs as "add-on" to opt/jobotwar. it executes fine from there, anyway...
 
 # set java release to 14
-sed -i "" "s/\<maven\.compiler\.release>13\</<maven.compiler.release>14</" pom.xml
+#sed -i "" "s/\<maven\.compiler\.release>13\</<maven.compiler.release>14</" pom.xml
 # rebuild jars
 mvn clean package
 # package jars in dmg
 jpackage -i jobotwar-gui/target/ -n jobotwar --main-class net.smackem.jobotwar.gui.JarMain --main-jar $JAR --java-options '--enable-preview' --app-version $VERSION
 # reset java release
-sed -i "" "s/\<maven\.compiler\.release>14\</<maven.compiler.release>13</" pom.xml 
+#sed -i "" "s/\<maven\.compiler\.release>14\</<maven.compiler.release>13</" pom.xml 
