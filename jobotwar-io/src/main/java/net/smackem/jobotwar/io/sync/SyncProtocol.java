64eb5bb70cb11513e9b8fa030d78b962db2da72f
smackem <sm4ckem@outlook.com>
2020-05-12 22:24:07 +0200

    - sync messages: tests for TurnMessage

--- jobotwar-io/src/main/java/net/smackem/jobotwar/io/sync/SyncProtocol.java
+++ jobotwar-io/src/main/java/net/smackem/jobotwar/io/sync/SyncProtocol.java
@@ -3,34 +3,45 @@ package net.smackem.jobotwar.io.sync;
 import net.smackem.jobotwar.io.sync.messaging.Message;
 import net.smackem.jobotwar.io.tcp.Protocol;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 
 public class SyncProtocol implements Protocol<Message> {
+
+    private static final byte STX = 0x02;
+    private static final byte ETX = 0x03;
     private final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    private boolean inBody;
 
     @Override
     public Message readByte(byte b) {
-        switch (b) {
-            case 0x02 -> {
+        if (this.inBody) {
+            if (b == ETX) {
+                this.inBody = false;
                 return decodeMessage();
             }
-            case 0x03 -> {}
-            default -> bos.write(b);
+            bos.write(b);
+            return null;
+        }
+        if (b == STX) {
+            this.inBody = true;
         }
         return null;
     }
 
     @Override
     public ByteBuffer encodeMessage(Message message) {
-        //final String text;
-        //return ByteBuffer.wrap(text.getBytes(StandardCharsets.UTF_8));
-        return null;
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bos.write(STX);
+        message.encode(bos);
+        bos.write(ETX);
+        return ByteBuffer.wrap(bos.toByteArray());
     }
 
     private Message decodeMessage() {
         final byte[] bytes = this.bos.toByteArray();
         this.bos.reset();
-        return null;
+        return Message.decode(new ByteArrayInputStream(bytes));
     }
 }
