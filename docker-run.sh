2a9b235aa8abed0340ecc0d8ad6a404e34fbe911
smackem <sm4ckem@outlook.com>
2021-07-13 18:26:47 +0200

    - abstraction in docker shell script

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,20 @@
+DB_LOCAL_PATH=/home/pbo/jobotwar/jobotwar-web/target/db
+DB_SCHEMA=jobotwar
+DB_USER=philip
+DB_PWD=jobotwar
+
+docker build --tag jobotwar .
 docker network create jobotwar-net
 docker run -dp 5432:5432 \
-    -v /home/pbo/jobotwar/jobotwar-web/target/db:/var/lib/postgresql/data \
-    -e POSTGRES_PASSWORD=jobotwar \
-    -e POSTGRES_USER=philip \
-    -e POSTGRES_DB=jobotwar \
-    -e PGDATA=/var/lib/postgresql/data/jobotwar \
+    -v $DB_LOCAL_PATH:/var/lib/postgresql/data \
+    -e POSTGRES_PASSWORD=$DB_PWD \
+    -e POSTGRES_USER=$DB_USER \
+    -e POSTGRES_DB=$DB_SCHEMA \
+    -e PGDATA=/var/lib/postgresql/data/$DB_SCHEMA \
     --name jobotwar-postgres \
     --network jobotwar-net
     postgres:latest
 docker run -dp 8666:8666 \
-    -e DBURL='jdbc:postgresql://jobotwar-postgres/jobotwar?user=philip&password=jobotwar' \
+    -e DBURL='jdbc:postgresql://jobotwar-postgres/$DB_SCHEMA?user=$DB_USER&password=$DB_PWD' \
     --network jobotwar-net \
     jobotwar
