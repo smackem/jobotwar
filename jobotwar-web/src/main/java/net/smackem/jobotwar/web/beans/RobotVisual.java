a4dd6bf18b49ffe37adf0f0ae81174b1f431e807
smackem <10erSaiten>
2021-08-25 17:35:08 +0200

    - added CORS to api

--- jobotwar-web/src/main/java/net/smackem/jobotwar/web/beans/RobotVisual.java
+++ jobotwar-web/src/main/java/net/smackem/jobotwar/web/beans/RobotVisual.java
@@ -9,21 +9,18 @@ public class RobotVisual {
     @JsonProperty private final double x;
     @JsonProperty private final double y;
     @JsonProperty private final String name;
-    @JsonProperty private final String color;
 
     @JsonCreator
     private RobotVisual() {
         this.x = 0;
         this.y = 0;
         this.name = null;
-        this.color = null;
     }
 
-    public RobotVisual(double x, double y, String name, String color) {
+    public RobotVisual(double x, double y, String name) {
         this.x = x;
         this.y = y;
         this.name = Objects.requireNonNull(name);
-        this.color = Objects.requireNonNull(color);
     }
 
     public double x() {
@@ -36,9 +33,5 @@ public class RobotVisual {
 
     public String name() {
         return this.name;
-    }
-
-    public String color() {
-        return this.color;
     }
 }
