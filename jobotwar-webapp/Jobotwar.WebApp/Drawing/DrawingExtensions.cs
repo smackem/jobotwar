ef411c1333c053aceb7100f0403e134a45f898c5
smackem <sm4ckem@outlook.com>
2021-10-24 22:22:23 +0200

    - little optimization for css rgba regex

--- jobotwar-webapp/Jobotwar.WebApp/Drawing/DrawingExtensions.cs
+++ jobotwar-webapp/Jobotwar.WebApp/Drawing/DrawingExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -6,6 +7,8 @@ namespace Jobotwar.WebApp.Drawing
 {
     internal static class Rgba
     {
+        private static readonly Regex HexPattern = new(@"\#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})", RegexOptions.Compiled);
+
         public static string ToCssRgba(this Color rgba)
         {
             return $"rgba({rgba.R}, {rgba.G}, {rgba.B}, {rgba.A / 255.0})";
@@ -17,8 +20,13 @@ namespace Jobotwar.WebApp.Drawing
         }
 
         public static (byte r, byte g, byte b) ParseCssColor(string cssColor)
-        {
-            var match = Regex.Match(cssColor, @"\#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})");
+        { 
+            var match = HexPattern.Match(cssColor);
+            if (match.Success == false)
+            {
+                throw new ArgumentException("unsupported format", nameof(cssColor));
+            }
+
             return (byte.Parse(match.Groups[1].Value, NumberStyles.HexNumber),
                 byte.Parse(match.Groups[2].Value, NumberStyles.HexNumber),
                 byte.Parse(match.Groups[3].Value, NumberStyles.HexNumber));
